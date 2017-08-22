bc689777fecf976d37a691823ccdef128d20e620
Dan Guest <dguest@cern.ch>
2017-08-22 20:35:35 +0200

    basic things working

--- test-network.py
+++ test-network.py
@@ -9,9 +9,11 @@ _help_hdf5_file = "NN weights file from Keras"
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from numpy import linspace
+from numpy.lib.recfunctions import merge_arrays
 import numpy as np
 import json
 from math import isnan
+from h5py import File
 
 def _get_args():
     parser = ArgumentParser(
@@ -22,6 +24,23 @@ def _get_args():
     parser.add_argument('hdf5_file', help=_help_hdf5_file)
     parser.add_argument('data_file', help='run on this file')
     return parser.parse_args()
+
+
+class Preprocessor:
+    def __init__(self, inputs):
+        n_inputs = len(inputs)
+        self.scale = np.zeros((n_inputs,))
+        self.offset = np.zeros((n_inputs,))
+        self.default = np.zeros((n_inputs,))
+        self.pos_dict = {}
+        for nnn, entry in enumerate(inputs):
+            pos_dict[entry['name']] = nnn
+            self.scale[nnn] = entry['scale']
+            self.offset[nnn] = entry['offset']
+            self.default[nnn] = entry['default']
+
+    def get_array(self, struct_array):
+        normed_values = (raw_values + offset) * scale
 
 
 def run():
@@ -40,29 +59,39 @@ def run():
     n_inputs = model.layers[0].input_shape[1]
     assert n_inputs == len(inputs['inputs'])
 
-    for pat in generate_test_pattern(args.data_file, input_dict=inputs):
-        outputs = list(model.predict(pat))[0]
-        print(outputs)
-
-def generate_test_pattern(input_file, input_dict):
-
     pos_dict = {}
     scale = np.zeros((n_inputs,))
     offset = np.zeros((n_inputs,))
-    for nnn, entry in enumerate(input_dict['inputs']):
+    for nnn, entry in enumerate(inputs['inputs']):
         pos_dict[entry['name']] = nnn
         scale[nnn] = entry['scale']
         offset[nnn] = entry['offset']
 
-    
-    for key, value in zip(field_keys, field_values):
-        input_pos = pos_dict[key]
-        if isnan(value):
-            value = input_dict['inputs'][input_pos]['default']
-        raw_values[input_pos] = value
 
-    normed_values = (raw_values + offset) * scale
-    
+    for pat in generate_test_pattern(args.data_file, input_dict=inputs):
+
+        print(pat.dtype.names)
+        return
+        # normed_values = (raw_values + offset) * scale
+
+        # outputs = list(model.predict(pat))[0]
+        # print(outputs)
+
+def generate_test_pattern(input_file, input_dict, chunk_size=2):
+
+    with File(input_file, 'r') as h5file:
+        for start in range(0, h5file['jets'].shape[0], chunk_size):
+            sl = slice(start, start + chunk_size)
+            subjets = []
+            for sjn in [1,2]:
+                subjet = np.asarray(h5file[f'subjet{sjn}'][sl])
+                newnames = [f'subjet_{sjn}_{nm}' for nm in subjet.dtype.names]
+                subjet.dtype.names = newnames
+                subjets.append(subjet)
+
+            yield merge_arrays((h5file['jets'][sl],*subjets), flatten=True)
+
+
 
 if __name__ == '__main__':
     run()
