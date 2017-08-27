39315c4556b97d67ebf1ae28253baa4f24457f1b
Julian Collado <colladou@uci.edu>
2017-08-27 12:05:35 -0700

    created skeleton for tests

---
+++ test_evaluate_network.py
@@ -0,0 +1,18 @@
+
+
+def test_2D_ndarray_to_numpy():
+    pass
+
+def test_3D_ndarray_to_numpy():
+    pass
+
+def test_offset():
+    pass
+
+def test_scaling():
+    pass
+
+def test_missing_value_replacement():
+    pass
+
+
