b77164d1cca7ddccfd99f082b0c9a578bc027de8
Julian Collado <colladou@uci.edu>
2017-08-27 18:07:53 -0700

    updated run script for new names

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./test_network.py nn_data/architecture.json\
+./evaluate_network.py nn_data/architecture.json\
                   nn_data/hl_tracks_variable_description.json\
                   nn_data/weights.h5\
                   input_data/test_input.h5
