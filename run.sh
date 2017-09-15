710e08c23e5ea04a9e3e77f14db8a73e43458ab2
Dan Guest <dguest@cern.ch>
2017-09-15 19:46:14 +0200

    run the version of the script that works for me

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./evaluate_network.py nn_data/architecture.json\
+./test_evaluate_network.py nn_data/architecture.json\
                   nn_data/hl_tracks_variable_description.json\
                   nn_data/weights.h5\
                   input_data/small_test_categorized_data_signal.h5
