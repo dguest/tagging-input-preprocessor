f23e6579a562570a230da8ca4c76fc9f3cd299a6
Julian Collado <colladou@uci.edu>
2017-09-21 17:18:48 -0700

    changed input data file to test_data.h5 instead of smaller sample

--- get_predictions.sh
+++ get_predictions.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 cd input_data
-python3 flatten_hdf5.py raw_data.h5
-python3 split_into_categories.py
+rm test_data.h5
+python flatten_hdf5.py small_test_raw_data_signal.h5
+python split_into_categories.py
 rm temporary_flattened_data.h5
 cd ..
 
 ./evaluate_network.py nn_data/architecture.json\
                   nn_data/hl_tracks_variable_description.json\
                   nn_data/weights.h5\
-                  input_data/test_input.h5
+                  input_data/test_data.h5
