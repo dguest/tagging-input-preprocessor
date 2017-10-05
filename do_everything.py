cdd7a7381c3f2e78cc555ee7ef5fcc35587e37d2
Dan Guest <dguest@cern.ch>
2017-10-05 13:11:41 +0200

    full (simplified) pipeline working for hl tracks

--- do_everything.py
+++ do_everything.py
@@ -34,70 +34,15 @@ def flatten_3d_into_2d(data):
         return data
 
 def sub_select_data(data, start, end, sub_selection):
-    if len(data.shape) == 1:
-        return data[start:end]
-    elif len(data.shape) == 2:
-        return data[start:end, sub_selection]
-    elif len(data.shape) == 3:
-        return data[start:end, sub_selection, :]
+    return data[:,sub_selection,...]
 
 def copy_in_batches(data, save_data, sub_selection):
-    assert data is not None
-    assert save_data is not None
-
-    rounded_num_samples = save_data.shape[0]
-    batch_size = 100
-
-    if data is list:
-        assert len(data) == len(sub_selection), "a sub_selection must be specified for each dataset"
-
-    for start, end in zip(range(0, rounded_num_samples, batch_size),
-                          range(batch_size, rounded_num_samples+batch_size, batch_size)):
-        if data.__class__ is list:
-            assert len(data) == len(sub_selection), "a sub_selection must be specified for each dataset"
-            if len(data) == 2:
-                data_1 = data[0]
-                data_2 = data[1]
-                sub_selection_1 = sub_selection[0]
-                sub_selection_2 = sub_selection[1]
-
-                temp_data_1 = sub_select_data(data_1, start, end, sub_selection_1)
-                temp_data_2 = sub_select_data(data_2, start, end, sub_selection_2)
-
-                temp_data_1 = flatten_3d_into_2d(temp_data_1)
-                temp_data_2 = flatten_3d_into_2d(temp_data_2)
-
-                mini_batch = np.hstack((temp_data_1, temp_data_2))
-
-            elif len(data) == 3:
-                data_1 = data[0]
-                data_2 = data[1]
-                data_3 = data[2]
-                sub_selection_1 = sub_selection[0]
-                sub_selection_2 = sub_selection[1]
-                sub_selection_3 = sub_selection[2]
-                temp_data_1 = sub_select_data(data_1, start, end, sub_selection_1)
-                temp_data_2 = sub_select_data(data_2, start, end, sub_selection_2)
-                temp_data_3 = sub_select_data(data_3, start, end, sub_selection_3)
-                temp_data_1 = flatten_3d_into_2d(temp_data_1)
-                temp_data_2 = flatten_3d_into_2d(temp_data_2)
-                temp_data_3 = flatten_3d_into_2d(temp_data_3)
-                merged_1_2 = np.hstack((temp_data_1, temp_data_2))
-                mini_batch = np.hstack((merged_1_2, temp_data_3))
-
-        else:
-            mini_batch = sub_select_data(data, start, end, sub_selection)
-            mini_batch = flatten_3d_into_2d(mini_batch)
-
-        num_dims = len(mini_batch.shape)
-
-        if num_dims == 1:
-            save_data[start:end] = mini_batch
-        elif num_dims == 2:
-            save_data[start:end, :] = mini_batch
-        elif num_dims == 3:
-            save_data[start:end, :, :] = mini_batch
-
+    batch_size = save_data.shape[0]
+    batches = []
+    for dat, subsel in zip(data, sub_selection):
+        subsel_dat = sub_select_data(dat, 0, 0, subsel)
+        batches.append(flatten_3d_into_2d(subsel_dat))
+    save_data[...] = np.hstack(batches)
 
 def get_high_level_tracks(input_file, output_file):
     sub_selection_0 = range(0, 2)
