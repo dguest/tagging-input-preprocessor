13a03e0b424256ac9c957a17f329bdbfd9439901
Dan Guest <dguest@cern.ch>
2017-10-05 15:25:09 +0200

    refactor the feature extraction a bit

--- do_everything.py
+++ do_everything.py
@@ -47,47 +47,45 @@ def get_flat_array(input_file, output_file_name):
 # ________________________________________________________________________
 # Part 2
 
-def flatten_3d_into_2d(data):
-    if len(data.shape) == 3:
-        reshaped_data = data.reshape(data.shape[0], data.shape[1]*data.shape[2])
-        return reshaped_data
-    else:
-        return data
-
-def sub_select_data(data, start, end, sub_selection):
-    return data[:,sub_selection,...]
-
-def copy_in_batches(data, save_data, sub_selection):
-    batch_size = save_data.shape[0]
-    batches = []
+def get_subselection(data, sub_selection):
+    batch_size = data[0].shape[0]
+    slices = []
     for dat, subsel in zip(data, sub_selection):
-        subsel_dat = sub_select_data(dat, 0, 0, subsel)
-        batches.append(flatten_3d_into_2d(subsel_dat))
-    save_data[...] = np.hstack(batches)
+        # take a slice of the data, and then reshape it to flatten
+        # everything past the first dimenstion
+        subsel_dat = dat[:,subsel].reshape(dat.shape[0], -1)
+        slices.append(subsel_dat)
+    return np.hstack(slices)
 
-def get_high_level_tracks(input_file, output_file):
-    sub_selection_0 = range(0, 2)
-    sub_selection_1 = list(range(0,15)) + list(range(16, 34)) + list(range(36, 40))
-    sub_selection_2 = list(range(0,15)) + list(range(16, 34)) + list(range(36, 40))
-    feature_name = 'hl_tracks'
-    with File(input_file) as open_file:
-        data_0 = open_file['jets/']
-        data_1 = open_file['subjet1/']
-        data_2 = open_file['subjet2/']
-        N = data_1.shape[0]
+sub_selections = {
+    'hl_tracks': [
+        ('jets', range(0, 2)),
+        ('subjet1', list(range(0,15)) + list(range(16, 34)) + list(range(36, 40)) ),
+        ('subjet2', list(range(0,15)) + list(range(16, 34)) + list(range(36, 40))),
+    ]
+}
 
-        with File(output_file, 'w') as save_file:
-            save_data = save_file.create_dataset(feature_name, (N, 2+37*2),
-                                                 dtype=float)
-            copy_in_batches([data_0, data_1, data_2], save_data, [sub_selection_0, sub_selection_1, sub_selection_2])
+
+def get_features(input_file, output_file, feature_name):
+    ds_names, selection_lists = zip(*sub_selections[feature_name])
+    data_list = [input_file[nm] for nm in ds_names]
+    N = data_list[0].shape[0]
+
+    with File(output_file, 'w') as save_file:
+        n_features = sum([len(l) for l in selection_lists])
+        save_data = get_subselection(data_list, selection_lists)
+        save_file.create_dataset(feature_name, (N, n_features),
+                                 data=save_data, dtype=float)
+
 
 
 def run():
     input_file = "small_test_raw_data_signal.h5"
-    flat_file = "temporary_flattened_data_dan.h5"
+    flat_file_name = "temporary_flattened_data_dan.h5"
     test_file = "test_data_dan.h5"
-    get_flat_array(input_file, flat_file)
-    get_high_level_tracks(flat_file, test_file)
+    get_flat_array(input_file, flat_file_name)
+    with File(flat_file_name) as flat_file:
+        get_features(flat_file, test_file, feature_name='hl_tracks')
 
 if __name__ == '__main__':
     run()
